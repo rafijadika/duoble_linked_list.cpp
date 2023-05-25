7452ef2bf214e5d09fbaa7bf570cff7f88bb2a65
rafijadika <jadika.rafi@gmail.com>
2023-05-25 12:00:04 +0700

    add note

--- duoble_linked_list.cpp/duoble_linked_list.cpp.cpp
+++ duoble_linked_list.cpp/duoble_linked_list.cpp.cpp
@@ -1,3 +1,11 @@
-DoubleLinkedList::DoubleLinkedList() {
-	START = NULL;
-}
+void DoubleLinkedList::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student: ";
+	cin >> nim;
+	cout << "/nEnter tha name of the student: ";
+	cin.ignore();
+	getline(cin, nm);
+	Node* newNode = new Node();
+	newNode->noMhs = nim;
+	newNode->name = nm;
