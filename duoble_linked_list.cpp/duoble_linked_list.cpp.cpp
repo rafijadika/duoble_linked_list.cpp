8d6910f70c7e8efba4c6993745eff5ffbefdbdb3
rafijadika <jadika.rafi@gmail.com>
2023-05-25 18:54:22 +0700

    commit 11

--- duoble_linked_list.cpp/duoble_linked_list.cpp.cpp
+++ duoble_linked_list.cpp/duoble_linked_list.cpp.cpp
@@ -1,7 +1,3 @@
-bool DoubleLinkedList::search(int rollNO, Node** previous, Node** current) {
-	*previous = *current = START;
-	while (*current != NULL && rollNO != (*current)->noMhs) {
-		*previous = *current;
-		*current = (*current)->next;
-	}
-	return(*current != NULL);
+bool DoubleLinkedList::listEmpty() {
+	return (START == NULL);
+}
