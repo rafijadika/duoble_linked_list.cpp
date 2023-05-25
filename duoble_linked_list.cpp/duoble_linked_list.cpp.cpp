3c6a0e54e7eb905f1451377401205312ce099445
rafijadika <jadika.rafi@gmail.com>
2023-05-25 18:50:42 +0700

    commit 7

--- duoble_linked_list.cpp/duoble_linked_list.cpp.cpp
+++ duoble_linked_list.cpp/duoble_linked_list.cpp.cpp
@@ -1,12 +1,3 @@
-if (START == NULL || nim <= START->noMhs) {
-	if (START != NULL && nim == START->noMhs) {
-		cout << "\nDuplicate number not allowed" << endl;
-		return;
-	}
-	newNode->next = START;
-	if (START != NULL)
-		START->prev = newNode;
-	newNode->prev = NULL;
-	START = newNode;
-	return;
-}
+Node* current = START;
+while (current->next != NULL && nim > current->next->noMhs)
+current = current->next;
