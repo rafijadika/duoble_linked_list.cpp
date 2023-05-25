7dd16d12e29fd8a2d252d480a38e04bd29e710b2
rafijadika <jadika.rafi@gmail.com>
2023-05-25 18:55:43 +0700

    commit 14

--- duoble_linked_list.cpp/duoble_linked_list.cpp.cpp
+++ duoble_linked_list.cpp/duoble_linked_list.cpp.cpp
@@ -1,3 +1,12 @@
-void DoubleLinkedList::descending() {
-	if (listEmpty())
-		cout << "\nlist is empty" << endl;
+else {
+	cout << "\nRecord in descending order of roll number are: " << endl;
+	Node* currentNode = START;
+	while (currentNode->next != NULL) {
+		currentNode = currentNode->next;
+	}
+	while (currentNode != NULL) {
+		cout << currentNode->noMhs << " " << currentNode->name << endl;
+		currentNode = currentNode->prev;
+	}
+	}
+}
