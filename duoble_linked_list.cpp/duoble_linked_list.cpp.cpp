45374560542e92c198b6f4a6a71e01ebd0c8d15b
rafijadika <jadika.rafi@gmail.com>
2023-05-25 18:56:53 +0700

    commit 17

--- duoble_linked_list.cpp/duoble_linked_list.cpp.cpp
+++ duoble_linked_list.cpp/duoble_linked_list.cpp.cpp
@@ -1,12 +1,14 @@
-else {
-	cout << "\nRecord in descending order of roll number are: " << endl;
-	Node* currentNode = START;
-	while (currentNode->next != NULL) {
-		currentNode = currentNode->next;
-	}
-	while (currentNode != NULL) {
-		cout << currentNode->noMhs << " " << currentNode->name << endl;
-		currentNode = currentNode->prev;
-	}
-	}
+void DoubleLinkedList::hapus() {
+	if (listEmpty())
+		cout << "\nlist is empty" << endl;
+
+	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+	int rollNo;
+	cin >> rollNo;
+	cout << endl;
+
+	if (DoubleLinkedList::deleteNode(rollNo) == false)
+		cout << "Record not found" << endl;
+	else
+		cout << "Record with roll number " << rollNo << " deleted" << endl;
 }
