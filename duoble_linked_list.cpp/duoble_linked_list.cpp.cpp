bf6aea088acd2d9c0e4c22fb353f59944e09653b
rafijadika <jadika.rafi@gmail.com>
2023-05-25 11:59:32 +0700

    double linked lost

--- duoble_linked_list.cpp/duoble_linked_list.cpp.cpp
+++ duoble_linked_list.cpp/duoble_linked_list.cpp.cpp
@@ -1,15 +1,3 @@
-class DoubleLinkedList {
-private:
-	Node* START;
-public:
-
-	DoubleLinkedList();
-	void addNode();
-	bool search(int rollNo, Node** previous, Node** current);
-	bool deleteNode(int rollNo);
-	bool listEmpty();
-	void ascending();
-	void descending();
-	void hapus();
-	void searchData();
-}; 
+DoubleLinkedList::DoubleLinkedList() {
+	START = NULL;
+}
